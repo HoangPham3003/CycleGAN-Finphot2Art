f0556ea344cfbf34130c04f4c5099e6b7e2ff030
HoangPham3003 <hoang.ph303@gmail.com>
2024-11-13 21:40:07 +0000

    update main

--- main.py
+++ main.py
@@ -5,53 +5,21 @@ import subprocess
 
 from tqdm.auto import tqdm
 
+from datasets.download import load_data
+from datasets.dataloader import ArtLoader
+
 if __name__ == '__main__':
-    
     ROOT_DIR = os.getcwd()
-    ROOT_DATA = os.path.join(ROOT_DIR, 'DATA')
-    DATA_NAME = 'vangogh2photo'
-    DATA_DIR = os.path.join(ROOT_DATA, DATA_NAME)
+    
+    DATA_DIR = load_data(ROOT_DIR)
+    
+    loader = ArtLoader(DATA_DIR=DATA_DIR)
+    train_loader, test_loader = loader.load_data()
+    
+    print(len(train_loader))
+    print(len(test_loader))
     
     
-    chk = False
-    if not os.path.exists(DATA_DIR):
-        script_file = os.path.join(ROOT_DIR, 'datasets', 'download.sh')
-        print(f"Executing download script: {script_file}")
-        
-        if not os.path.isfile(os.path.join(ROOT_DATA, f"{DATA_NAME}.zip")):
-            print(f"\nDownloading zipfile dataset {DATA_NAME}\n")
-            try:
-                with subprocess.Popen(
-                    ['bash', '-x', script_file, DATA_NAME],
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
-                    text=True
-                ) as proc:
-                    pbar = tqdm(total=100, desc=f"Downloading", unit="%")
-
-                    for line in proc.stderr:
-                        match = re.search(r'(\d+)%', line)
-                        if match:
-                            percentage = int(match.group(1))
-                            pbar.n = percentage
-                            pbar.refresh()
-
-                    proc.wait()
-                    pbar.n = 100  # Set progress bar to complete at 100%
-                    pbar.refresh()
-                    pbar.close()
-                
-                print("Shell script executed successfully.")
-                
-            except subprocess.CalledProcessError as e:
-                print("Error occurred while executing the shell script.")
-                print(e.stderr.decode())
-        
-        # Unzip the downloaded file in Python
-        zip_path = os.path.join(ROOT_DATA, f"{DATA_NAME}.zip")
-        print(f"\nUnzipping {zip_path}...")
-        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-            zip_ref.extractall(ROOT_DATA)
-        print("Dataset is ready!")
+    
         
     
