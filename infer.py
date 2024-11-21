5493319171f41ee9c8eb8792ada8b613a6654036
HoangPham3003 <hoang.ph303@gmail.com>
2024-11-21 22:18:43 +0000

    add argparse

--- infer.py
+++ infer.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
@@ -22,49 +23,46 @@ def infer(pretrained_path, image_path, infer_transforms_, save_dir, device):
         return
     
     print("Loading pretrained...")
-    pretrained_model = torch.load(pretrained_path)
+    pretrained_model = torch.load(pretrained_path, weights_only=True)
     gen_BA.load_state_dict(pretrained_model['gen_BA'])
     print("Pretrained model is ready!")
     
     transform = transforms.Compose(infer_transforms_)
     image = Image.open(image_path)
     image = transform(image)
-    image = nn.functional.interpolate(image, size=256)
-    
     image = image.to(device)
     
     with torch.no_grad():
         fake_A = gen_BA(image)
-    output_fake = fake_A.cpu().detach().numpy()
-    plt.imshow(output_fake)
+    output_fake = ((fake_A + 1) / 2) * 255
+    output_fake = output_fake.cpu().detach().numpy()
+    output_fake = output_fake.transpose(1, 2, 0)
+    output_fake = cv2.cvtColor(output_fake, cv2.COLOR_BGR2RGB)
     
+    image_name = image_path.split('/')[-1].split('.')[0]
     if not os.path.exists(save_dir):
         os.mkdir(save_dir)
-    plt.savefig(f"{save_dir}/infer.jpg")
-    print("The result is saved at : {}".format(f"{save_dir}/infer.jpg"))
+    save_path = f"{save_dir}/infer_{image_name}.jpg"
+    work_dir = os.getcwd()
+    save_path = os.path.join(work_dir, save_path)
+    cv2.imwrite(save_path, output_fake)
+    print("The result is saved at : {}".format(save_path))
     
     
 if __name__ == '__main__':
-    
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    
-    work_dir = os.getcwd()
-    save_dir = f"{work_dir}/inference"
-    
-    image_dir = f"{work_dir}/DATA_INFER"
-    pretrained_dir = f"{work_dir}/pretrained"
-    
-    pretrained_path = f"{pretrained_dir}/CycleGAN.pt"
-    print(pretrained_path)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-pt", "--pretrained", type=str, default="pretrained/CycleGAN.pt", help="name of the pretrained model")
+    parser.add_argument("-ip", "--image_path", type=str, default="DATA_INFER/img1.jpg", help="infered image path")
+    parser.add_argument("-sd", "--save_dir", type=str, default="inference", help="name of save folder")
+    parser.add_argument("-d", "--device", type=str, default="cuda", help="cuda or cpu")
+    args = parser.parse_args()
     
     infer_transforms_ = [
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ]
     
-    image_path = f"{image_dir}/img1.jpg"
-    
-    infer(pretrained_path, image_path, infer_transforms_, save_dir, device)
+    infer(args.pretrained, args.image_path, infer_transforms_, args.save_dir, args.device)
     
     
     
