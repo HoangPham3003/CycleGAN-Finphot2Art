d67623e5b933dbd05edd0d49c501c80376102b61
HoangPham3003 <hoang.ph303@gmail.com>
2024-11-21 22:18:02 +0000

    update argparse

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import argparse
 import numpy as np
 from tqdm.auto import tqdm
 
@@ -15,33 +16,37 @@ from losses import get_disc_loss, get_gen_loss
 
 if __name__ == '__main__':
     
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-pt", "--pretrained", type=str, default="pretrained/CycleGAN.pt", help="name of the pretrained model")
+    parser.add_argument("-lr", "--learning_rate", type=int, default=0.0002, help="learning rate")
+    parser.add_argument("-ep", "--epochs", type=int, default=20, help="number of epochs")
+    parser.add_argument("-bs", "--batch_size", type=int, default=1, help="the size of each training batch")
+    parser.add_argument("-ds", "--display_step", type=int, default=200, help="number of steps for tracking")
+    parser.add_argument("-ts", "--target_shape", type=int, default=256, help="the size of input image")
+    parser.add_argument("-d", "--device", type=str, default="cuda", help="cuda or cpu")
+    parser.add_argument("-s", "--save", type=bool, default=True, help="save model or not")
+    args = parser.parse_args()
+    
+    
     ### =============== Hyper-parameters =============== ### 
     
-    # save_dir = '/content/drive/MyDrive/LEARNING/CV/CycleGAN' # GoogleColab
-    save_dir = '/kaggle/working' # Kaggle
-    work_dir = os.getcwd()
-    pretrained_dir = f"{work_dir}/pretrained"
-    
+    ROOT_DIR = os.getcwd()
     dim_A = 3
     dim_B = 3
-    n_epochs = 20
-    display_step = 200
-    batch_size = 1
-    lr = 0.0002
-    load_shape = 286
-    target_shape = 256
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    print(device)
+    n_epochs = args.epochs
+    display_step = args.display_step
+    batch_size = args.batch_size
+    lr = args.learning_rate
+    target_shape = args.target_shape
+    device = args.device
+    print("Device : ", device)
+    
+    save_model = args.save
     
     adv_criterion = nn.MSELoss()
     recon_criterion = nn.L1Loss()
     
-    save_model = True
-    
-    
     ### =============== Load data =============== ###
-    
-    ROOT_DIR = os.getcwd()
     
     DATA_DIR = down_data(ROOT_DIR)
     
@@ -75,9 +80,9 @@ if __name__ == '__main__':
         betas=(0.5, 0.999)
     )
     
-    pretrained = False
-    if pretrained:
-        pre_dict = torch.load(f'{pretrained_dir}/CycleGAN.pt')
+    if args.pretrained != '':
+        print("Loading pretrained...")
+        pre_dict = torch.load(args.pretrained, weights_only=True)
         gen_AB.load_state_dict(pre_dict['gen_AB'])
         gen_BA.load_state_dict(pre_dict['gen_BA'])
         gen_opt.load_state_dict(pre_dict['gen_opt'])
@@ -85,6 +90,7 @@ if __name__ == '__main__':
         disc_A_opt.load_state_dict(pre_dict['disc_A_opt'])
         disc_B.load_state_dict(pre_dict['disc_B'])
         disc_B_opt.load_state_dict(pre_dict['disc_B_opt'])
+        print("Pretrained model is ready!")
     else:
         gen_AB = gen_AB.apply(weights_init)
         gen_BA = gen_BA.apply(weights_init)
@@ -97,9 +103,8 @@ if __name__ == '__main__':
     mean_generator_loss = 0
     mean_discriminator_loss = 0
     cur_step = 0
-    if not os.path.exists(f"{save_dir}/history"):
-        os.mkdir(f"{save_dir}/history")
-    f = open(f"{save_dir}/history/train_log.txt", 'a')
+    if not os.path.exists(f"{ROOT_DIR}/log"):
+        os.mkdir(f"{ROOT_DIR}/log")
     
     for epoch in range(n_epochs):
         for real_A, real_B in tqdm(train_loader):
@@ -140,13 +145,14 @@ if __name__ == '__main__':
             if cur_step % display_step == 0:
                 print(f"Epoch {epoch}: Step {cur_step}: Generator (U-Net) loss: {mean_generator_loss}, Discriminator loss: {mean_discriminator_loss}")
                 
+                f = open(f"{ROOT_DIR}/log/train_log.txt", 'a')
                 f.write(f"Epoch {epoch}: Step {cur_step}: Generator (U-Net) loss: {mean_generator_loss}, Discriminator loss: {mean_discriminator_loss}\n")
+                f.close()
                 
                 show_tensor_images(torch.cat([real_A, real_B]), size=(dim_A, target_shape, target_shape), current_step=cur_step, real=True)
                 show_tensor_images(torch.cat([fake_B, fake_A]), size=(dim_B, target_shape, target_shape), current_step=cur_step, real=False)
                 mean_generator_loss = 0
                 mean_discriminator_loss = 0
-                # You can change save_model to True if you'd like to save the model
                 if save_model:
                     torch.save({
                         'gen_AB': gen_AB.state_dict(),
@@ -156,6 +162,5 @@ if __name__ == '__main__':
                         'disc_A_opt': disc_A_opt.state_dict(),
                         'disc_B': disc_B.state_dict(),
                         'disc_B_opt': disc_B_opt.state_dict()
-                    }, f"{save_dir}/CycleGAN.pt")
+                    }, f"{ROOT_DIR}/CycleGAN.pt")
             cur_step += 1
-    f.close()
